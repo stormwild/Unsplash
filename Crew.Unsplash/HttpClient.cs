7310773e849383267641096c7f4dd8d73623e6ef
Alexander Torrijos <artorrijos@gmail.com>
2019-08-14 12:18:08 +0800

    Initial commit

---
+++ Crew.Unsplash/HttpClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Crew.Unsplash
+{
+    public class HttpClient
+    {
+        private readonly HttpClient _client;
+
+        public HttpClient(HttpClient client)
+        {
+            _client = client;
+        }
+    }
+}
